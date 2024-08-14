b0f414cea1ccce979c01b783bd91ef7bfc6fa394
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2024-08-14 18:51:43 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className='flex justify-center items-center h-screen underline'>
+      <div className='text-3xl'>
+        hello world
+      </div>
+    </div>
+  )
+}
+
+export default App
